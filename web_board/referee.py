2aaa9d7237fc56e917694f2cb59c648bf942dc67
Bruno Morais <brunosmmm@gmail.com>
2019-05-20 19:49:33 -0400

    more

--- web_board/referee.py
+++ web_board/referee.py
@@ -19,12 +19,13 @@ from web_board.ipc import ScoreboardIPCError
 bp = Blueprint("referee", __name__)
 
 
-def web_ipc_call(ipc_type, **deco_kwargs):
+def web_ipc_call(**deco_kwargs):
     """Web API decorator to write less."""
 
     def wrapper(func):
         @functools.wraps(func)
         async def wrapped(*args, **kwargs):
+            ipc_type = func.__name__
             try:
                 data = await ipc.do_ipc_call(ipc_type, **deco_kwargs)
                 if isinstance(data, dict):
@@ -69,13 +70,13 @@ async def index():
 
 
 @bp.route("/status/game")
-@web_ipc_call("game_status")
+@web_ipc_call()
 async def game_status(data):
     """Get game status."""
 
 
 @bp.route("/status/players")
-@web_ipc_call("player_status")
+@web_ipc_call()
 async def player_status(data):
     """Get player status."""
 
@@ -94,12 +95,30 @@ async def game_can_start():
 
 
 @bp.route("/control/gbegin")
-@web_ipc_call("game_begin")
-async def start_game(data):
+@web_ipc_call()
+async def game_begin(data):
     """Start the game."""
 
 
 @bp.route("/control/gend")
-@web_ipc_call("game_end")
-async def end_game(data):
+@web_ipc_call()
+async def game_end(data):
     """End the game."""
+
+
+@bp.route("/persist/tournament/<tid>")
+async def activate_tournament(tid):
+    """Activate tournament."""
+    try:
+        await ipc.activate_tournament(tournament_id=tid)
+        data = {"status": "ok"}
+    except ScoreboardIPCError:
+        data = {"status": "error"}
+
+    return jsonify(data)
+
+
+@bp.route("/persist/tournament_off")
+@web_ipc_call()
+async def deactivate_tournament():
+    """Deactivate tournament."""
